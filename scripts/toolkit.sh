a7054da6a538c8767db2353eb1ad6353a78afce9
Sawors <yt.sawors@gmail.com>
2024-04-05 00:03:18 +0200

    added a linux wrapped script

---
+++ scripts/toolkit.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+repo=https://github.com/Sawors/dreams/archive/refs/heads/master.zip
+download_dir=$(mktemp -d)
+src_dir=./install
+entry_point=__main__.py
+entry_point_path=${src_dir}/src/${entry_point}
+toolkit_args=--interactive
+
+# not using embedded python on Linux
+
+if test -f "${entry_point_path}";
+then
+    echo Upgrading toolkit...
+else
+    echo Installing toolkit...
+fi
+
+curl -LJ "$repo" -o "$download_dir/toolkit.zip"
+unzip "$download_dir/toolkit.zip" -d "$download_dir"
+mkdir -p "$src_dir"
+rm -r "${src_dir}/src"
+mv "${download_dir}/dreams-master/src" "$src_dir"
+rm -r "$download_dir"
+
+if test -f "${entry_point_path}";
+then
+    echo Toolkit successfully installed !
+
+    if ! /usr/bin/env python --version 2>&1 > /dev/null;
+    then
+        rm -r "${src_dir}/src"
+        echo Python not found, aborting.
+        exit 9009
+    fi
+    echo Automatically starting its execution...
+    /usr/bin/env python "$entry_point_path" $toolkit_args
+else
+    echo Download failed, aborting.
+    exit 1
+fi
